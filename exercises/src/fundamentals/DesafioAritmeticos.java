c928e18d9658fdc3bac8134432b6890d1fadc083
lutrazovski <lutrazovski@gmail.com>
2025-09-10 21:50:44 -0300

    Desafio de Aritmetica

---
+++ exercises/src/fundamentals/DesafioAritmeticos.java
@@ -0,0 +1,11 @@
+package fundamentals;
+
+public class DesafioAritmeticos {
+    public static void main(String[] args) {
+        double a = Math.pow((6 * (3 + 2)), 2) / (3 * 2);
+        double b = Math.pow((double) ((1 - 5) * (2 - 7)) / 2 , 2);
+        double c = a - b;
+        double d = Math.pow(c , 3) / Math.pow(10, 3);
+        System.out.println(d);
+    }
+}
